0b0345d12a93d406e9d373f92074c8440aa44f91
Victor Ross <victor2ross@gmail.com>
2022-03-03 17:13:19 -0300

    Primeiro commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './global.css';
+
+function App() {
+  return (
+    <div className="app">
+      <header className="app-header">
+        <a href="/">amazona</a>
+      </header>
+      <main>list products</main>
+    </div>
+  );
+}
+
+export default App;
